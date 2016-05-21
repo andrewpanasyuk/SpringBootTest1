3433194537dfda396051d67409a33327eab54596
Andrew <avpk77@gmail.com>
2016-05-21 21:28:58 +0300

    Initial commit

---
+++ src/main/java/test/photogallary/handler.java
@@ -0,0 +1,8 @@
+package test.photogallary;
+
+/**
+ * Created by a_pan on 21.05.2016.
+ */
+public class handler {
+
+}
---
+++ test/java/test.photogallary/Aplication.java
@@ -0,0 +1,7 @@
+//package test.photogallary;
+//
+///**
+// * Created by a_pan on 21.05.2016.
+// */
+//public class Aplication {
+//}
